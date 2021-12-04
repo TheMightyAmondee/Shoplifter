1e11f8b2cf5d2f44d4d13d1686e0c720afa994f5
Amelie <amelie.parkinson.bates@gmail.com>
2021-12-04 17:02:23 +1100

    begin translation support

---
+++ Shoplifter/i18n.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+
+
+namespace Shoplifter
+{
+    internal static class i18n
+    {
+        private static ITranslationHelper translation;
+        private static IManifest manifest;
+        private static ModConfig config;
+        public static void gethelpers(ITranslationHelper translation)
+        {
+            i18n.translation = translation;
+        }
+
+        public static string string_Banned()
+        {
+            return i18n.GetTranslation("TheMightyAmondee.Shoplifter/Banned");
+        }
+
+        public static string string_Caught(string shopkeeper)
+        {
+            return i18n.GetTranslation($"TheMightyAmondee.Shoplifter/Caught{shopkeeper}");
+        }
+
+        public static string string_Caught_NoMoney(string shopkeeper)
+        {
+            return i18n.GetTranslation($"TheMightyAmondee.Shoplifter/Caught{shopkeeper}_NoMoney");
+        }
+
+        public static string string_BanFromShop()
+        {
+            return i18n.GetTranslation("TheMightyAmondee.Shoplifter/BanFromShop");
+        }
+
+        public static string string_AlreadyShoplifted()
+        {
+            return i18n.GetTranslation("TheMightyAmondee.Shoplifter/AlreadyShoplifted");
+        }
+
+        public static string string_AlreadyShopliftedSameShop()
+        {
+            return i18n.GetTranslation("TheMightyAmondee.Shoplifter/AlreadyShopliftedSameShop");
+        }
+
+        public static Translation GetTranslation(string key, object tokens = null)
+        {
+            if (i18n.translation == null)
+                throw new InvalidOperationException($"You must call {nameof(i18n)}.{nameof(i18n.gethelpers)} from the mod's entry method before reading translations.");
+
+            return i18n.translation.Get(key, tokens);
+        }
+    }
+}
